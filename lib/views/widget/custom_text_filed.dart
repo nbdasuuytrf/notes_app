af72eabb613527f1d283e9b6f6e684cdb347e602
nasr <nasralmahshmi@gmial.com>
2026-09-26 20:42:50 +0300

    add Edit note view ui done

--- lib/views/widget/custom_text_filed.dart
+++ lib/views/widget/custom_text_filed.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:noteapp/constanse_color.dart';
 
 class CustomTextFiled extends StatelessWidget {
-  const CustomTextFiled({super.key, required this.hint, this.maxLines = 1});
+  const CustomTextFiled({super.key, required this.hint, this.maxLines = 1,this.hintStyle});
   final String hint;
   final int maxLines;
+  final TextStyle? hintStyle;
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +14,7 @@ class CustomTextFiled extends StatelessWidget {
       maxLines: maxLines,
       decoration: InputDecoration(
         hintText: hint,
-    
+
         hintStyle: TextStyle(color: kPrimaryColor),
         border: bulidBorder(),
         enabledBorder: bulidBorder((kPrimaryColor)),
@@ -24,7 +25,7 @@ class CustomTextFiled extends StatelessWidget {
 
   OutlineInputBorder bulidBorder([Color]) {
     return OutlineInputBorder(
-       borderRadius: BorderRadius.circular(16),
+      borderRadius: BorderRadius.circular(16),
       borderSide: BorderSide(color: Color ?? Colors.white),
     );
   }
--- lib/views/widget/edit_view_body.dart
+++ lib/views/widget/edit_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:noteapp/views/widget/coustom_app_bar.dart';
+import 'package:noteapp/views/widget/custom_text_filed.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
@@ -12,6 +13,10 @@ class EditNoteViewBody extends StatelessWidget {
         children: const [
           SizedBox(height: 50),
           CoustomAppbar(title: 'Edit Note', icon: Icons.check),
+          SizedBox(height: 50),
+          CustomTextFiled(hint: 'Titel'),
+          SizedBox(height: 24),
+          CustomTextFiled(hint: "Content", maxLines: 10,hintStyle: TextStyle(),),
         ],
       ),
     );

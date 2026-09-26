6b75f925d6c4855861af745014fa6e8a97da9b82
nasr <nasralmahshmi@gmial.com>
2026-09-26 20:57:53 +0300

    create  note model

---
+++ lib/model/note_model.dart
@@ -0,0 +1,7 @@
+class NoteModel {
+  final String title;
+  final String subtitle;
+  final String date;
+  final int color;
+  NoteModel({ required this.title,required this.subtitle,required this.color,required this.date});
+}
